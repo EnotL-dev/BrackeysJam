4726b363da9be9a22ba82092400af8ae2832ee58
Seven <ngodaloc2006@gmail.com>
2026-08-26 20:47:19 +0700

    add npccontroller animation

---
+++ Assets/_game/Npc/Animation/NPCAnimationController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets._game.Npc.Animation {
+    public class NPCAnimationController {
+
+        private readonly Animator animator;
+
+        private static readonly int LocomotionState =
+        Animator.StringToHash("LocomotionState");
+
+        private static readonly int ActionState =
+        Animator.StringToHash("ActionState");
+
+        public NPCAnimationController( Animator animator ) {
+            this.animator = animator;
+        }
+
+        public void SetLocomotion( NPCActionState state ) {
+            animator.SetInteger(LocomotionState, (int)state);
+        }
+
+        public void SetAction( NPCActionState state ) {
+            animator.SetInteger(ActionState, (int)state);
+        }
+
+
+
+
+    }
+}
