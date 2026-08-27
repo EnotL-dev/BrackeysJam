4485362ee4dbea5332459723fa5722bfb2d0d1e2
Seven <ngodaloc2006@gmail.com>
2026-08-27 15:19:15 +0700

    integrate with vibe chance

--- Assets/_game/Npc/Controller/NPCInfoGenerator.cs
+++ Assets/_game/Npc/Controller/NPCInfoGenerator.cs
@@ -1,33 +1,40 @@
-﻿using Assets._game.Bar.Model.Alcohol;
+﻿using Assets._game.Bar.Controller;
+using Assets._game.Bar.Model.Alcohol;
 using Assets._game.Npc.Enum;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using Zenject;
 
 namespace Assets._game.Npc.Controller {
     public class NPCInfoGenerator {
 
         string[] names = { "John", "Alex", "Mike", "David", "James" };
         string[] sexes = { "Male", "Female" };
-        NPCProperty[] nPCProperties;
+        NPCProperty[] npcProperties;
         NPCWealthType[] wealthTypes;
         AlcoholType[] alcoholTypes;
+
+        IBarService barService;
+
+        [Inject]
+        void Construct( IBarService barService ) {
+            this.barService = barService;
+        }
 
         public NPCInfo Generate() {
             string name = names[UnityEngine.Random.Range(0, names.Length)];
             int age = UnityEngine.Random.Range(10, 80);
             string sex = sexes[UnityEngine.Random.Range(0, sexes.Length)];
 
-            if ( wealthTypes == null ) wealthTypes = (NPCWealthType[])System.Enum.GetValues(typeof(NPCWealthType));
-            if ( nPCProperties == null ) nPCProperties = (NPCProperty[])System.Enum.GetValues(typeof(NPCProperty));
-            if ( alcoholTypes == null ) alcoholTypes = (AlcoholType[])System.Enum.GetValues(typeof(AlcoholType));
+            wealthTypes ??= (NPCWealthType[])System.Enum.GetValues(typeof(NPCWealthType));
+            npcProperties ??= (NPCProperty[])System.Enum.GetValues(typeof(NPCProperty));
+            alcoholTypes ??= (AlcoholType[])System.Enum.GetValues(typeof(AlcoholType));
 
             var wealth = wealthTypes[UnityEngine.Random.Range(0, wealthTypes.Length)];
 
-            //This random should be influence by vibe status
-            NPCProperty nPCProperty = nPCProperties[UnityEngine.Random.Range(0, nPCProperties.Length)]; //TODO: this should be stack
-            List<NPCProperty> properties = new ();
-            properties.Add(nPCProperty);
+            List<NPCProperty> properties = GenerateProperties(barService.GetVibe().vibe);
 
             //foreach ( var property in nPCProperties ) {
             //    npcProperties.Add( property );
@@ -54,6 +61,21 @@ namespace Assets._game.Npc.Controller {
                 properties,
                 farDrink
             );
+        }
+
+        private List<NPCProperty> GenerateProperties( float currentVibe ) {
+            var selectedProperties = new HashSet<NPCProperty>();
+
+            // Roll for how many traits this NPC gets (e.g., 1 to 2 traits)
+            int traitCount = UnityEngine.Random.Range(1, 3);
+
+            // TODO: Replace this loop with your vibe-based probability weights later
+            while ( selectedProperties.Count < traitCount && selectedProperties.Count < npcProperties.Length ) {
+                NPCProperty randomProperty = npcProperties[UnityEngine.Random.Range(0, npcProperties.Length)];
+                selectedProperties.Add(randomProperty);
+            }
+
+            return selectedProperties.ToList();
         }
     }
 }
